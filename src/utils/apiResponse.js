619ab20a572660c5cbc9283c1cac14fd472d9e0c
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-13 11:21:50 +0600

    Added api response

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,11 @@
+class ApiResposne {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+
+}
+
+export default ApiResposne
