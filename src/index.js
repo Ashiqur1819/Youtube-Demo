931e293f6a8716e8410e35f1dc018f724b9d73b4
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-13 10:54:27 +0600

    Added async handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from "./db/index.js";
+import app from './app.js';
+const port = process.env.PORT || 3000
 
 // Databse connection
 connectDB()
+.then(() => {
+
+    app.on("error", (error) => {
+        console.log("Error", error)
+        throw error
+    })
+
+    app.get("/", async(req, res) => {
+        res.send("Server is running")
+    })
+
+    app.listen(port, () => {
+        console.log("Server is running on port: ", port)
+    })
+
+})
+.catch((err) => {
+    console.log("MongoDB Connection ERROR: ", err)
+})
