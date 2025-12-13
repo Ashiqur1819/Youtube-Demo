1278f8bff581ded000a2eaeab94e68b4f9de7f5e
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-13 12:11:46 +0600

    Added video schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,47 +1,50 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     watchHistory: [
-        {
-            type: mongoose.Schema.Types.ObjectId(),
-            ref: "Video"
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId(),
+        ref: "Video",
+      },
     ],
     userName: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     fullName: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
     avatar: {
-        type: String, // From Claudinary
-        required: true
+      type: String, // From Claudinary
+      required: true,
     },
     coverImage: {
-        type: String, // From Claudinary
+      type: String, // From Claudinary
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     refreshToken: {
-        type: String,
-    }
-}, {timestamps: true})
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-export const User = mongoose.model("User", userSchema)
+export const User = mongoose.model("User", userSchema);
